b5f6a02856713ea994e870222dbb04ee808211f6
James Cuzella <james.cuzella@lyraphase.com>
2012-05-03 17:29:31 -0600

    Fixes for supporting longer hex ciphertext strings. (split into blocks and encrypt each)
    Chop last char off for hex strings with odd length.

--- vncpasswd.py
+++ vncpasswd.py
@@ -5,6 +5,9 @@ import d3des as d
 import argparse
 #from struct import pack, unpack
 
+def split_len(seq, length):
+    return [seq[i:i+length] for i in range(0, len(seq), length)]
+
 def do_crypt(password, decrypt):
     passpadd = (password + '\x00'*8)[:8]
     strkey = ''.join([ chr(x) for x in d.vnckey ])
@@ -27,6 +30,18 @@ def do_file_out(filename, data, inhex):
     f.write(data)
     f.close()
 
+def hex(s):
+    try:
+        s = s.decode('hex')
+    except TypeError as e:
+        if e.message == 'Odd-length string':
+            print 'WARN: %s . Chopping last char off... "%s"' % ( e.message, s[:-1] )
+            s = s[:-1].decode('hex')
+        else:
+            raise
+    return s
+
+
 def main():
     parser = argparse.ArgumentParser(description='Encrypt or Decrypt a VNC password')
     parser.add_argument("-d", "--decrypt", dest="decrypt", action="store_true", default=False, \
@@ -44,11 +59,30 @@ def main():
     if ( args.filename == None and args.passwd == None ):
         parser.error('Error: No password file or password passed\n')
     if ( args.passwd != None and args.hex ):
-	    args.passwd = args.passwd.decode('hex')
+        args.passwd = hex(args.passwd)
     if ( args.filename != None and args.decrypt ):
         args.passwd = do_file_in(args.filename, args.hex)
 
-    crypted = do_crypt(args.passwd, args.decrypt)
+    # If the hex encoded passwd length is longer than 16 hex chars and divisible
+    # by 16, then we chop the passwd into blocks of 64 bits (16 hex chars)
+    # (1 hex char = 4 binary bits = 1 nibble)
+    hexpasswd = args.passwd.encode('hex')
+    if ( len(hexpasswd) > 16 and (len(hexpasswd) % 16) == 0 ):
+        splitstr = split_len(args.passwd.encode('hex'), 16)
+        print 'splitstr = %s' % splitstr
+        cryptedblocks = []
+        for sblock in splitstr:
+            cryptedblocks.append( do_crypt(sblock.decode('hex'), args.decrypt) )
+            print '%016s\t%s' % ( sblock, cryptedblocks )
+            crypted = ''.join(cryptedblocks)
+    elif ( len(hexpasswd) <= 16):
+        crypted = do_crypt(args.passwd, args.decrypt)
+    else:
+        if ( args.decrypt ):
+            print 'WARN: Ciphertext length was not divisible by 8 (hex/16).'
+            print 'Length: %d' % len(args.passwd)
+            print 'Hex Length: %d' % len(hexpasswd)
+        crypted = do_crypt(args.passwd, args.decrypt)
 
     if ( args.filename != None and not args.decrypt ):
         do_file_out(args.filename, crypted, args.hex)

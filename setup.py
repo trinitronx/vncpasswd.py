fd61b9ab3cecb5727490a4f54d1d4377ce70813d
James Cuzella <james.cuzella@lyraphase.com>
2018-10-17 16:34:42 -0600

    Add setup.py for distutils packaging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='vncpasswd.py',
+    version='1.2.0',
+    packages=['d3des','WindowsRegistry',],
+    license='License :: OSI Approved :: MIT License',
+    long_description=open('README.md').read(),
+)

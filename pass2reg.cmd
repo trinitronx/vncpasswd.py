68fe7265419e8f0e6d2d8e37ca1be8fec53278ee
James Cuzella <james.cuzella@lyraphase.com>
2020-06-21 05:31:55 -0600

    fix(Windows): Update usage help output for pass2reg.cmd script

--- pass2reg.cmd
+++ pass2reg.cmd
@@ -1,11 +1,15 @@
 @echo off
 setlocal
-if "%1" == "" (
-    echo usage: %0 ^<password^>
-    exit /b
-)
 set regkey=HKLM\SOFTWARE\ORL\WinVNC3\Default
 set valname=Password
+if "%1" == "" (
+    echo usage: %0 ^<password^>
+    echo           Write ^<password^> to Windows Registry
+    echo           Destinations:
+    echo             %regkey%\%valname%
+    echo             %regkey%\%valname%2
+    exit /b
+)
 
 rem clumsy cmd way to set password hash to env var
 for /f "delims=" %%H in ('vncpasswd.py -o -e %1') do set hash=%%H

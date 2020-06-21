bc594a562cdf1d3c86990f666198492d8d98c9b4
James Cuzella <james.cuzella@lyraphase.com>
2020-06-21 05:25:43 -0600

    fix(Windows): Fix pass2reg.cmd script for WinVNC3 registry write

--- pass2reg.cmd
+++ pass2reg.cmd
@@ -8,7 +8,7 @@ set regkey=HKLM\SOFTWARE\ORL\WinVNC3\Default
 set valname=Password
 
 rem clumsy cmd way to set password hash to env var
-for /f "delims=" %%H in ('vnc_hash.py %1') do set hash=%%H
+for /f "delims=" %%H in ('vncpasswd.py -o -e %1') do set hash=%%H
 
 echo VNC Hash is: %hash%
 echo Writing to: %regkey%\%valname%
